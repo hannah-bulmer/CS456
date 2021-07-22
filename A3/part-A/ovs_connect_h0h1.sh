8d5628cb8d2a7010c7ee2f5b0182f58697977bd1
Hannah Bulmer <hannah.bulmer@elastic.io>
2021-07-22 14:40:00 -0400

    fix?

--- A3/part-A/ovs_connect_h0h1.sh
+++ A3/part-A/ovs_connect_h0h1.sh
@@ -17,17 +17,17 @@ done
 ofctl='ovs-ofctl -O OpenFlow13'
 
 # OVS rules for switch 0
-$ofctl add-flow s0 \ # send from s0-eth2 to s1-eth2
+$ofctl add-flow s0 \
     in_port=1,ip,nw_src=10.0.0.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:0A:01:00:02,mod_dl_dst:0A:00:0A:FE:00:02,output=2 
 
-$ofctl add-flow s0 \ # send from s0-eth1 to h0-eth0
+$ofctl add-flow s0 \
     in_port=2,ip,nw_src=10.0.1.2,nw_dst=10.0.0.2,actions=mod_dl_src:0A:00:00:01:00:01,mod_dl_dst:0A:00:00:02:00:00,output=1 
 
 # OVS rules for switch 1
-$ofctl add-flow s1 \ # send from s1-eth1 to h1-eth0
+$ofctl add-flow s1 \
     in_port=2,ip,nw_src=10.0.0.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output=1 
 
-$ofctl add-flow s1 \ # send from s1-eth2 to s0-eth2
+$ofctl add-flow s1 \
     in_port=1,ip,nw_src=10.0.1.2,nw_dst=10.0.0.2,actions=mod_dl_src:0A:00:0A:FE:00:02,mod_dl_dst:0A:00:0A:01:00:02,output=2 
 
 # Print the flows installed in each switch
