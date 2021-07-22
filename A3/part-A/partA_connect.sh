5d3b00bca6ad13ec118b3df8673edaae8a8da094
Hannah Bulmer <hannah.bulmer@elastic.io>
2021-07-22 14:43:28 -0400

    Remove comments

--- A3/part-A/partA_connect.sh
+++ A3/part-A/partA_connect.sh
@@ -30,15 +30,18 @@ ofctl='ovs-ofctl -O OpenFlow13'
 
 # --------------------------------------------------- h2 to h4
 # link from h2 to s2 to s3
-$ofctl add-flow s2 \ # send from s2 to s3
-    in_port=1,ip,nw_src=10.0.2.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:0C:FE:00:04,mod_dl_dst:0A:00:0D:FE:00:02,output=4
+# send from s2 to s3
+$ofctl add-flow s2 \
+    in_port=2,ip,nw_src=10.0.0.2,nw_dst=10.0.1.2,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output=1
 
 # link from s3 to s4
-$ofctl add-flow s2 \ # come from 2, send from s3 to s4 on 3
+# come from 2, send from s3 to s4 on 3
+$ofctl add-flow s2 \
     in_port=2,ip,nw_src=10.0.2.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:0E:01:00:03,mod_dl_dst:0A:00:0E:FE:00:02,output=3
 
 # link from s4 to h4
-$ofctl add-flow s2 \ # come from 2, send from s4 to h4 on 1
+# come from 2, send from s4 to h4 on 1
+$ofctl add-flow s2 \
     in_port=2,ip,nw_src=10.0.2.2,nw_dst=10.0.4.2,actions=mod_dl_src:0A:00:04:01:00:01,mod_dl_dst:0A:00:04:02:00:00,output=1
 
 
