fc71032d50ba1c5830b4def1e2e41965f105cff9
Hannah Bulmer <hannah.bulmer@elastic.io>
2021-07-30 18:39:28 -0400

    Fri 30 Jul 2021 18:39:28 EDT

--- A3/part-B/partB_connect.sh
+++ A3/part-B/partB_connect.sh
@@ -27,31 +27,51 @@ ofctl='ovs-ofctl -O OpenFlow13'
 
 #  ------------------- Alice talks to Carol -----------------------
 # connect s0 to r1
+$ofctl add-flow s0 \
+    in_port=1,actions=output:2
 
-# r1 to r1
 
-# connect r1 to s1
+# r1 to s1
+$ofctl add-flow r1 \
+    in_port=1,ip,nw_src=10.1.1.17,nw_dst=10.6.6.69,actions=mod_dl_src:0A:00:0E:FE:00:02,mod_dl_dst:0A:00:01:01:00:01,output=2
+
 
 # connect s1 to r2
+$ofctl add-flow s1 \
+    in_port=1,actions=output:3
 
-# r2 to r2
 
-# connect r2 to s2
+# r2 to s2
+$ofctl add-flow r2 \
+    in_port=1,ip,nw_src=10.1.1.17,nw_dst=10.6.6.69,actions=mod_dl_src:0A:00:10:FE:00:02,mod_dl_dst:0A:00:02:01:00:01,output=2
+
 
 # connect s2 to carol
+$ofctl add-flow s2 \
+    in_port=1,actions=output:2
 
 #  ------------------- Carol talks to Alice -----------------------
 
 # connect s2 to r2
+$ofctl add-flow s2 \
+    in_port=2,actions=output:1
 
-# r2 to r2
 
-# connect r2 to s1
+# r2 to s1
+$ofctl add-flow r2 \
+    in_port=2,ip,nw_src=10.6.6.69,nw_dst=10.1.1.17,actions=mod_dl_src:0A:00:05:01:00:01,mod_dl_dst:0A:00:0C:01:00:03,output=1
+
 
 # connect s1 to r1
+$ofctl add-flow s1 \
+    in_port=3,actions=output:1
 
-# r1 to r1
 
-# connect r1 to s0
+# r1 to s0
+$ofctl add-flow r1 \
+    in_port=2,ip,nw_src=10.6.6.69,nw_dst=10.1.1.17,actions=mod_dl_src:0A:00:04:01:00:01,mod_dl_dst:0A:00:0A:01:00:02,output=1
+
 
 # connect s0 to alice
+$ofctl add-flow s0 \
+    in_port=2,actions=output:1
